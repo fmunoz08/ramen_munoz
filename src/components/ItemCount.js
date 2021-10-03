e18615c09dc19c92c72c81218b36a8a0b15b5083
yosz328 <44147404+yosz328@users.noreply.github.com>
2021-10-03 16:07:07 -0300

    Added ItemCount

---
+++ src/components/ItemCount.js
@@ -0,0 +1,70 @@
+import { style } from "@mui/system";
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const DivControl = styled.div`
+    display: flex;
+    flex-direction: row;
+    
+`;
+
+const Background = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 16px;
+`;
+
+const Numero = styled.div`
+    border: 1px solid gray;
+    border-radius: 16px;
+    width: 100px;
+    text-align: center;
+
+`;
+
+const Boton = styled.button`
+    width: 50px;
+    color: blue;
+    font-size: 18px;
+
+`;
+
+const LargeBoton = styled.button`
+color: blue;
+font-size: 18px;
+margin: 16px;
+`;
+
+function ItemCount ({ stock, initial, onAdd}) {
+
+    const [number, setNumber] = useState(initial);
+
+    function AddQuantity(){
+        if (number < stock)
+            setNumber(number + 1)
+
+    }
+
+    function LessQuantity(){
+        if (number > 0)
+            setNumber(number - 1)
+
+    }
+
+    return (
+        <Background>
+            <p>Lorem Ipsum</p>
+            <DivControl>
+                <Boton onClick={() => LessQuantity()}> - </Boton>         
+                <Numero> {number} </Numero>
+                <Boton onClick={() => AddQuantity()}> + </Boton>
+            </DivControl>
+            <LargeBoton>Agregar al carrito</LargeBoton>
+
+            
+            
+        </Background>
+    );
+}
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,15 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+import ItemCount from "./ItemCount";
+
 const Title = styled.h1`
     text-align: center;
 `;
+
+
+const Background = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
 
 const ItemListContainer = ({greeting}) => {
 
 
     return(
+        <Background>
         <Title>{greeting}</Title>
+        <ItemCount stock={5} initial={1}/>
+        </Background>
     );
 }
 
